b34e6ffe658b4fafb9a36ac9b4a08063826c234e
Gene Hazan <ghazan@microsoft.com>
2018-03-23 15:35:00 -0700

    Re-write and simplify

--- index.js
+++ index.js
@@ -1,50 +1,21 @@
 module.exports = npm1k
 
-const concatSeries = require('async.concatseries')
-const https = require('https')
-const uniq = require('array-uniq')
+const requestRetry = require('requestretry').defaults({ json: true, maxAttempts: 3, fullResponse: false })
 
-function getOffsets(limit) {
-  const offsets = []
-  for (let i = 0; i <= limit; i += 36) {
-    offsets.push(i)
-  }
-  return offsets
+function npm1k(callback, limit = 1000) {
+  process(limit)
+    .then(names => callback(null, names))
+    .catch(error => callback(error))
 }
 
-function npm1k(callback, limit = 1050) {
-  const offsets = getOffsets(limit)
-  concatSeries(
-    offsets,
-    (offset, callback) => {
-      getMostDependedPage(offset, (error, data) => {
-        if (error) {
-          callback(error) }
-        else {
-          callback(null, packageNames(data)) } }) },
-          (error, packages) => {
-      if (error) {
-        callback(error) }
-      else {
-        callback(null, uniq(packages).slice(0, 1000)) } }) }
-
-function packageNames(data) {
-  const parsed = JSON.parse(data)
-  return parsed.packages.map(pkg => pkg.name)
-}
-
-function getMostDependedPage(offset, callback) {
-  https.get(
-    { hostname: 'www.npmjs.com',
-      path: '/browse/depended?offset=' + offset,
+async function process(limit) {
+  let names = []
+  for (let offset = 0; offset < limit; offset += 36) {
+    const response = await requestRetry.get(`https://www.npmjs.com/browse/depended?offset=${offset}`, {
       headers: { 'x-spiferack': 1 }
-    },
-    (response) => {
-      const buffers = []
-      response
-        .on('data', (buffer) => {
-          buffers.push(buffer) })
-        .on('error', (error) => {
-          callback(error) })
-        .on('end', () => {
-          callback(null, Buffer.concat(buffers).toString()) }) }) }
+    })
+    const namesPage = response.packages.map(pkg => pkg.name)
+    names = names.concat(namesPage)
+  }
+  return names.slice(0, limit)
+}
